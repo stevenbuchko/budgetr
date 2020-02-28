189b9c5d22154cc3881d42c14b254a1f34c74b50
Steven <stevenbuchko@gmail.com>
2020-02-27 18:30:38 -0600

    sort of fixed refresh issue

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -23,10 +23,10 @@ class HomeScreen extends React.Component<Props, State> {
         };
     }
 
-    async componentDidMount() {
+    async fetchData() {
         try {
             await axios.get('http://192.168.1.2:3000/api/v1/users/dc5bf63a-38d1-474e-b944-9a18e206a81e')
-                .then( res => {
+                .then(res => {
                     console.log(res.data);
                     const budget_amount = res.data.budget_amount;
                     const total_expenses = res.data.total_expenses;
@@ -35,9 +35,17 @@ class HomeScreen extends React.Component<Props, State> {
                     this.setState({ total_expenses });
                 })
                 .catch(err => console.log(err));
-            } catch (error) {
-                console.log('Fetch Error:', error)
-            }
+        } catch (error) {
+            console.log('Fetch Error:', error)
+        }
+    }
+
+    componentDidMount() {
+        this.fetchData();
+
+        this.props.navigation.addListener('didFocus', () => {
+            this.fetchData();
+        });
     }
     
     render() {
