248f00e2252c55158f70a7958b3b833e0baefa08
Steven <stevenbuchko@gmail.com>
2020-03-30 13:30:00 -0500

    added javascript injection to plaid webview

--- src/screens/AddWalletAccount/AddWalletAccountScreen.tsx
+++ src/screens/AddWalletAccount/AddWalletAccountScreen.tsx
@@ -1,21 +1,110 @@
 import React from "react";
-import { Text, View } from 'react-native';
 import styles from "./styles";
-import { WebView } from "expo";
-import { Button } from "native-base";
+import { Container } from "native-base";
+import PlaidAuth from "../PlaidAuth/PlaidAuth";
 
 export interface Props {
     navigation: any;
 }
 
-export interface State { }
+interface Account {
+    id: string;
+    name: string;
+    type: string;
+    subtype: string;
+    mask: string;
+}
+
+interface Institution {
+    name: string;
+    institution_id: string;
+}
+
+interface PlaidData {
+    link_session_id?: string;
+    public_token: string;
+    account_id?: string;
+    account?: Account;
+    accounts?: Account[];
+    institution?: Institution;
+}
+
+export interface State {
+    plaidData?: PlaidData;
+    error: string;
+    loading: boolean;
+}
 
 class AddWalletAccountScreen extends React.Component<Props, State> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: '',
+            loading: false,
+        };
+    }
+
+    handleOnPlaidLinkComplete(metadata) {
+        // Optionally capture Link flow events, streamed through
+        // this callback as your users connect an Item to Plaid.
+        // For example:
+        // eventName = "TRANSITION_VIEW"
+        // metadata  = {
+        //   link_session_id: "123-abc",
+        //   mfa_type:        "questions",
+        //   timestamp:       "2017-09-14T14:42:19.350Z",
+        //   view_name:       "MFA",
+        // }
+        console.log('metadata: ' + JSON.stringify(metadata));
+        console.log('SETTING STATE');
+
+        if (!!metadata && !!metadata.public_token && metadata.public_token.length > 0) {
+            this.setState({
+                plaidData: metadata,
+            });
+        }
+    }
+
+    handleEventTracking(event) {
+        // Optionally capture Link flow events, streamed through
+        // this callback as your users connect an Item to Plaid.
+        // For example:
+        // eventName = "TRANSITION_VIEW"
+        // metadata  = {
+        //   link_session_id: "123-abc",
+        //   mfa_type:        "questions",
+        //   timestamp:       "2017-09-14T14:42:19.350Z",
+        //   view_name:       "MFA",
+        // }
+        /**
+         * eventName: TRANSITION_VIEW
+            metadata: {"error_code":null,"error_message":null,"error_type":null,
+            "exit_status":null,"institution_search_query":null,"mfa_type":null,"institution_name":"US Bank","
+            institution_id":"ins_6","link_session_id":"9b31fba1-407d-46fb-a344-9f4722018748","request_id":"smeONMwyxIifyd6",
+            "timestamp":"2019-05-18T23:08:53.273Z","view_name":"SELECT_ACCOUNT"}
+         */
+        console.log('eventName: ' + event.eventName);
+        console.log('metadata: ' + JSON.stringify(event.metadata));
+    }
+
+    handleOnExit() {
+        // handle the case when your user exits Link
+        console.log('Failed to link');
+        //
+        this.setState({
+            error: 'Please Retry',
+            loading: false
+        });
+    }
+
     render() {
         return (
-            <View style={styles.container}>
-                <Text>This is the add wallet account screen</Text>
-            </View>
+            <Container>
+                <PlaidAuth
+                    completePlaidLink={(metadata?: PlaidData) => this.handleOnPlaidLinkComplete(metadata)}
+                    handleEvent={(event?: any) => this.handleEventTracking(event)}
+                    navigation={this.props.navigation} />
+            </Container >
         )
     }
 }
--- src/screens/PlaidAuth/PlaidAuth.js
+++ src/screens/PlaidAuth/PlaidAuth.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import { WebView } from 'react-native-webview';
+import { Button, Text, View } from 'native-base';
+import styles from "./styles";
 
 const PLAID_PUBLIC_KEY = '54c75f7e9700d13893662d872beee7';
 const PLAID_ENV = 'sandbox';
 const PLAID_PRODUCT = 'transactions';
+const CLIENT_NAME = 'Budgetr';
+
+const injectedJavaScript = `(function() {
+  window.postMessage = function(data) {
+    window.ReactNativeWebView.postMessage(data);
+  };
+})()`;
 
 export default class PlaidAuth extends Component {
     constructor(props) {
         super(props);
         this.state = {
             data: {},
-            status: '',
+            status: 'LOGIN',
         };
     }
 
@@ -21,27 +30,40 @@ export default class PlaidAuth extends Component {
                 this.props.navigation.pop();
             case 'LOGIN':
             case 'plaid_link-undefined::exit':
-                return;
+                return this.renderSplash();
             default:
                 return this.renderTransactionConnection();
         }
+    }
+
+    renderSplash() {
+        return (
+            <View style={styles.container}>
+                <Text>This is the add wallet splash screen</Text>
+                <Button onPress={() => this.setState({ status: '' })}>
+                    <Text>Add New Account</Text>
+                </Button>
+            </View>
+        )
     }
 
     renderTransactionConnection() {
         return (
             <WebView
                 source={{
-                    uri: `https://cdn.plaid.com/link/v2/stable/link.html?key=${PLAID_PUBLIC_KEY}&env=${PLAID_ENV}&product=${PLAID_PRODUCT}&isWebView=true&isMobile=true`,
+                    uri: `https://cdn.plaid.com/link/v2/stable/link.html?key=${PLAID_PUBLIC_KEY}&env=${PLAID_ENV}&product=${PLAID_PRODUCT}&clientName=${CLIENT_NAME}&isWebView=true&isMobile=true`,
                 }}
                 onMessage={e => this.onMessage(e)}
+                injectedJavaScript={injectedJavaScript}
             />
-
         );
     }
 
 
     onMessage(e) {
+        console.log('in on message shit');
         if (!!!e || !!!e.nativeEvent) {
+            console.log('no e');
             return;
         }
         const encodedEventString = e.nativeEvent.data || null;
