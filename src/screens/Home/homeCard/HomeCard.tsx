e4621376e9cc986c5c979911ebb143c4062cd171
Steven <stevenbuchko@gmail.com>
2020-03-30 14:14:57 -0500

    added dates library and updated month to be current month

--- src/screens/Home/homeCard/HomeCard.tsx
+++ src/screens/Home/homeCard/HomeCard.tsx
@@ -1,30 +1,34 @@
-import { Text, Container, Header, Body, Card, Left, Right, Toast } from 'native-base';
+import { Text, Card, Right } from 'native-base';
+import { format } from 'date-fns'
 import * as React from 'react';
-import { Image, TouchableOpacity, View, ImageBackground, StyleSheet } from 'react-native';
+import { Image, TouchableOpacity, View } from 'react-native';
 import BudgetChart from './budgetChart/BudgetChart'
 import styles from './styles';
 
-export interface Props { 
+export interface Props {
     budget_amount: number;
     total_expenses: number;
     navigation: any;
 }
 
-export interface State { 
-    
+export interface State {
+
 }
 
 class HomeCard extends React.Component<Props, State> {
+
+    month = format(new Date(), 'MMMM');
+
     render() {
         return (
             <Card style={styles.cardContainer}>
                 <View style={styles.headerWrapper}>
                     <View style={styles.leftSide}>
                         <Text style={styles.forText}>for </Text>
-                        <Text style={styles.monthText}>Feburary</Text>
+                        <Text style={styles.monthText}>{this.month}</Text>
                     </View>
                     <Right style={styles.rightSide}>
-                        <TouchableOpacity onPress={() => this.props.navigation.navigate('EditBudget', 
+                        <TouchableOpacity onPress={() => this.props.navigation.navigate('EditBudget',
                             {
                                 budget_amount: this.props.budget_amount
                             }
@@ -33,7 +37,7 @@ class HomeCard extends React.Component<Props, State> {
                         </TouchableOpacity>
                     </Right>
                 </View>
-                <BudgetChart 
+                <BudgetChart
                     budget_amount={this.props.budget_amount}
                     total_expenses={this.props.total_expenses}
                 />
